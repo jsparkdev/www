99cd69f66836de278d7915f90fcf8b79039e98a0
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-06 01:37:49 +0900

    feat: tailwindcss@beta

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://jspark.dev",
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
   vite: {
     build: {
       target: "esnext",
     },
+    plugins: [tailwindcss()],
   },
 });
