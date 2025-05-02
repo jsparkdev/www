a1da5e53b8cd484535ddb6e2c0d20e8c286b5359
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-05-02 13:15:45 +0900

    chore: update contents and deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: "https://jspark.dev",
+  site: 'https://jspark.dev',
   vite: {
     plugins: [tailwindcss()],
     build: {
-      target: "esnext",
+      target: 'esnext',
     },
   },
   integrations: [sitemap()],
-});
+})
---
+++ src/const.ts
@@ -0,0 +1,22 @@
+export const TITLE = 'Junseong Park'
+export const DESCRIPTION = 'I\'m a Web Developer, Open Source Contributor, Product Engineer'
+export const SOCIAL_IMAGE_URL = 'https://jspark.dev/huston.jpg'
+
+export const SOCIAL_LINKS = [
+  {
+    title: 'Github',
+    url: 'https://github.com/jsparkdev',
+  },
+  {
+    title: 'X',
+    url: 'https://x.com/jsparkdev',
+  },
+  {
+    title: 'BlueSky',
+    url: 'https://bsky.app/profile/jspark.dev',
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://linkedin.com/in/jsparkdev',
+  },
+]
