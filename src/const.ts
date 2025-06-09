320450189c58eb3151b60504e09e1204a744e59f
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-06-09 17:23:10 +0900

    refactor: update

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const TITLE = 'Junseong Park'
-export const DESCRIPTION = 'I\'m a Web Developer, Open Source Contributor, Product Engineer'
-export const SOCIAL_IMAGE_URL = 'https://jspark.dev/huston.jpg'
+export const DESCRIPTION = 'I\'m Junseong Park, a full-stack web developer aiming to create and deliver the best experience possible.'
+export const SOCIAL_IMAGE_URL = 'https://jspark.dev/social.jpg'
 
 export const SOCIAL_LINKS = [
   {
