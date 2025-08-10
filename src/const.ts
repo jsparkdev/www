a1b97420b4244613d0b32fa4637610dc1ba4320a
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-08-10 13:50:09 +0900

    style: lint

--- src/const.ts
+++ src/const.ts
@@ -2,5 +2,5 @@ import { SITE_URL } from 'astro.config'
 
 const NAME = 'Junseong Park'
 export const TITLE = NAME
-export const DESCRIPTION = `${NAME}\'s Website`
+export const DESCRIPTION = `${NAME}\'s Personal Website`
 export const SOCIAL_IMAGE_URL = `${SITE_URL}/social.jpg`
